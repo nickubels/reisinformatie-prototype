4ea3eeaa3178184100768e2572d3681ed49a1a6f
Nick Ubels <nickubels@gmail.com>
2019-02-18 22:07:37 +0100

    Very basic tooltip with stopname

--- haltes/html/custom.js
+++ haltes/html/custom.js
@@ -18,12 +18,16 @@ var geojsonMarkerOptions = {
     fillOpacity: 0.8
 };
 
+function onEachFeature(feature, layer) {
+    layer.bindTooltip(feature.properties.quayname);
+}
+
 function loadMunicipality(code){
     jQuery.getJSON("../data/amsterdam.json", function(data) {
         console.log(data);
         L.geoJson(data, { pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, geojsonMarkerOptions);
-        }}).addTo(map);
+        }, onEachFeature: onEachFeature}).addTo(map);
     });
     // L.geoJSON(geojsonFeature).addTo(map);
 }
