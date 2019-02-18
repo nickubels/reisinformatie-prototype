2216361ad437abd6b72a29b84ce2d5f2302f1de9
Nick Ubels <nickubels@gmail.com>
2019-02-18 22:02:18 +0100

    Load Amsterdam data

--- haltes/html/custom.js
+++ haltes/html/custom.js
@@ -1,5 +1,7 @@
+var map;
+
 function initmap(){
-    var map = L.map('map');
+    map = L.map('map');
     var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
@@ -7,6 +9,21 @@ function initmap(){
     map.addLayer(osm);
 }
 
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
 function loadMunicipality(code){
-    
+    jQuery.getJSON("../data/amsterdam.json", function(data) {
+        console.log(data);
+        L.geoJson(data, { pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }}).addTo(map);
+    });
+    // L.geoJSON(geojsonFeature).addTo(map);
 }
